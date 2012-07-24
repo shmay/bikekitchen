37b5058ec2fcd6fffd4998813c488b14cd354aaf
Kyle Murphy <kmurph73@gmail.com>
2012-07-23 20:35:05 -0700

    blar

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sched::Application.config.secret_token = '9bc523df3a585f1682d2a30342fd087a3a3ad48fcb296af39ba051a5cee227bdac6bdf3e268ab095c68f2f876b587110b01f13d13c49dd8a5ee7a2f946928a20'
